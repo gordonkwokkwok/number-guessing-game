91c7b8cebf23b5135ee199b04880cd2f6aeb85dd
Gordon Kwok <112631794+gordonkwokkwok@users.noreply.github.com>
2022-11-20 12:21:26 +0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -5,26 +5,27 @@ def guess_range(x):
     guess = 0
     lower_limit = 0
     upper_limit = x
-    
+
     while guess != random_number:
         guess = int(input(f'Guess a number between {lower_limit} and {upper_limit}: '))
+
         if guess <= lower_limit or guess >= upper_limit:
+            print(f'Guessed number: {guess}')
             response = f'Guessed number is out of range! Guess again between {lower_limit} and {upper_limit}!'
-            print(response)
-            print(len(response) * '-')
-            
+
         elif guess < random_number:
             lower_limit = guess
+            print(f'Guessed number: {guess}')
             response = f'You guessed too small! Guess again between {lower_limit} to {upper_limit}!'
-            print(response)
-            print(len(response) * '-')
 
         elif guess > random_number:
             upper_limit = guess
+            print(f'Guessed number: {guess}')
             response = f'You guessed too high! Guess again between {lower_limit} to {upper_limit}!'
-            print(response)
-            print(len(response) * '-')
+
+        print(response)
+        print(len(response) * '-')
 
     print(f'Boom~! You have guessed the number {random_number} !!')
-    
-  guess_range(10)
+
+guess_range(10)
